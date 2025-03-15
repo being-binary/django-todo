947e8d0ccb2927f108dc3501f0292683295d6f1a
Abhishek Gupta <cryprincesis007@gmail.com>
2025-03-15 16:27:44 +0530

    user login logout

--- Todo/settings.py
+++ Todo/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'users.apps.UsersConfig',
 ]
 
 
@@ -130,3 +131,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
+LOGIN_REDIRECT_URL = 'homepage'
